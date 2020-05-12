5d7dae3f5d4e4dd589533ab017529ed0c5f55ca3
Muhammad Arham <arham.muhammad.017@gmail.com>
2020-05-12 03:09:28 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd
+import pickle
+
+filename = 'model.pkl'
+ensemble = pickle.load(open(filename, 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = vectorizer.transform(data).toarray()
+        pred = ensemble.predict(vect)
+    
+    return render_template('result.html', prediction = pred)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
